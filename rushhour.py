b3250f5fae0bd869815585c46809845cd1b03ec6
Peter Tsoi <petertsoi@gmail.com>
2011-09-12 00:40:15 -0700

    Finished basic heuristic.

--- rushhour.py
+++ rushhour.py
@@ -5,7 +5,25 @@
 # UC Berkeley
 
 from sets import Set
+from sets import ImmutableSet
 from sys import stdout
+from sys import argv
+import heapq
+
+class PriorityQueue:
+  def  __init__(self):  
+    self.heap = []
+    
+  def push(self, item, priority):
+      pair = (priority,item)
+      heapq.heappush(self.heap,pair)
+
+  def pop(self):
+      (priority,item) = heapq.heappop(self.heap)
+      return item
+  
+  def isEmpty(self):
+    return len(self.heap) == 0
 
 class Grid:
     def __init__(self, width, height, exitRow):
@@ -54,9 +72,19 @@ class Grid:
             moves.extend( v.validMoves())
         return moves
     
+    def state(self):
+        state = []
+        for v in self.traffic:
+            state.append((v.id, v.position))
+        state.sort()
+        return state
+    
+    def loadState(self, state):
+        for v in state:
+            self.getVehicle(v[0]).position = v[1]
+    
     def makeMove(self, move):
         v = self.getVehicle(move[0])
-        v.printInfo()
         v.move(move[1], move[2])
 
     # Prints the grid and the cars in it
@@ -91,7 +119,6 @@ class Grid:
         else:
             return False
 
-
 class Vehicle:
     def __init__(self, id, length, position, orientation, grid):
         self.id = id
@@ -121,7 +148,7 @@ class Vehicle:
             distance = 1
             start = self.position[0] if self.orientation == "right" else self.position[0] - self.length + 1
             while start > 1:
-                self.position = (start - 1, self.position[1])
+                self.position = (self.position[0] - 1, self.position[1])
                 if taken & self.occupiedSpaces() == Set():
                     validMoves.append([self.id, distance, "left"])
                 else:
@@ -133,7 +160,7 @@ class Vehicle:
             end = self.position[0] if self.orientation == "left" else self.position[0] + self.length - 1
             distance = 1
             while end < self.grid.width:
-                self.position = (end + 1, self.position[1])
+                self.position = (self.position[0] + 1, self.position[1])
                 if taken & self.occupiedSpaces() == Set():
                     validMoves.append([self.id, distance, "right"])
                 else:
@@ -146,7 +173,7 @@ class Vehicle:
             distance = 1
             start = self.position[1] if self.orientation == "down" else self.position[1] - self.length + 1
             while start > 1:
-                self.position = (self.position[0], start - 1)
+                self.position = (self.position[0], self.position[1] - 1)
                 if taken & self.occupiedSpaces() == Set():
                     validMoves.append([self.id, distance, "up"])
                 else:
@@ -158,7 +185,7 @@ class Vehicle:
             end = self.position[1] if self.orientation == "up" else self.position[1] + self.length - 1
             distance = 1
             while end < self.grid.height:
-                self.position = (self.position[0], end + 1)
+                self.position = (self.position[0], self.position[1] + 1)
                 if taken & self.occupiedSpaces() == Set():
                     validMoves.append([self.id, distance, "down"])
                 else:
@@ -189,15 +216,129 @@ class Vehicle:
     def printInfo(self):
         print ("Vehicle " + str(self.id) + "\tSize: 1 x " + str(self.length) + "\tAt: " + str(self.position) + "\tOrientation: " + str(self.orientation))
 
+class Search:
+    def __init__(self, grid):
+        self.fringe = PriorityQueue()
+        self.map = dict()
+        self.grid = grid
+        self.expandedNodes = 0
+    
+    def costOfMoves(self, moveList):
+        return len(moveList)
+
+    def aStarSearch(self):
+        # Start with initial state
+        initialState = self.grid.state()
+        self.map[str(initialState)] = []
+        for move in self.grid.allMoves():
+            g_cost = self.costOfMoves(self.map[str(initialState)]) + 1
+            h_cost = self.heuristic(move, initialState)
+            self.fringe.push((move, initialState), g_cost + h_cost)
+
+        # The actual algorithm
+        if self.grid.isFinished():
+            print("Expanded %i positions" % self.expandedNodes)
+            return self.map[str(self.grid.state())]
+        
+        while not self.fringe.isEmpty():
+            move = self.fringe.pop()
+            self.grid.loadState(move[1])
+            self.grid.makeMove(move[0])
+            newState = self.grid.state()
+            
+            # If this state has been visited
+            if str(newState) in self.map:
+                # If I took a shorter route to here, update it
+                #if self.costOfMoves(self.map[str(move[1])]) + 1 < self.costOfMoves(self.map[str(newState)]):
+                #    self.map[str(newState)] = self.map[str(move[1])].append(move[0])
+                # Then forget about it.
+                continue
+            else:
+                self.expandedNodes += 1
+                newMoveList = self.map[str(move[1])][:]
+                newMoveList.append(move[0])
+                self.map[str(newState)] = newMoveList
+                
+                if self.grid.isFinished():
+                    print("Expanded %i positions" % self.expandedNodes)
+                    self.grid.loadState(initialState)
+                    return self.map[str(newState)]
+                
+                for move in self.grid.allMoves():
+                    g_cost = self.costOfMoves(self.map[str(newState)]) + 1
+                    h_cost = self.heuristic(move, newState)
+                    self.fringe.push((move, newState), g_cost + h_cost)
+        print("Expanded %i positions" % self.expandedNodes)
+        print("Exhausted Fringe")
+        return[]
+    
+    def heuristic(self, successor, state):
+        # Rank by how many cars in between the special car and exit
+        score = 0
+        endOfSpecial = self.grid.special.position[0] + self.grid.special.length
+        blocking = []
+        for x in range(endOfSpecial, self.grid.width):
+            atLocation = self.grid.vehicleAt((x, self.grid.exitRow))
+            if atLocation != None:
+                score += 10
+                blocking.append(atLocation)
+        # Break ties by mobility of blocking cars
+        for v in blocking:
+            score -= 1 * len(v.validMoves())
+        return score
+    
+    def nullHeuristic(self, successor, state):
+        return 0
+        
+    def printSolution(self, moves):
+        initialState = self.grid.state()
+        print ("Solved in %i moves" % len(moves))
+        self.grid.printGrid()
+        print "\n"
+        for move in moves:
+            self.grid.makeMove(move)
+            self.grid.printGrid()
+            print "\n"
+
+def loadToGrid(path, grid):
+    f = open(path, 'r')
+    lines = f.readlines()
+    for line in lines:
+        args = line.split(' ')
+        coords = []
+        if ',' in args[2]:
+            coords = args[2].split(',')
+        coord = (int(coords[0]), int(coords[1]))
+        imported = Vehicle(args[0], int(args[1]), coord, args[3].rstrip(), grid)
+
+def writeToFile(path, moves):
+    f = open(path, 'w')
+    i = 1
+    for move in moves:
+        f.write(str(i) + " " + str(move[0]) + " " + str(move[1]) + " " + str(move[2]) + "\n")
+        i += 1
+
 def main():
+    printSolutions = False
+    if len(argv) != 3 or not (len(argv) == 4 and argv[1] == "-p"):
+        print "Usage:\t rushhour.py [-p] inputFile outputFile"
+        exit()
+    printSolutions = (argv[1] == "-p" and len(argv) == 4)
+    inPath = argv[2] if printSolutions else argv[1]
+    outPath = argv[3] if printSolutions else argv[2]
+            
     g = Grid(6, 6, 3)
-    truckA = Vehicle('A', 4, (1,2), "down", g)
-    carB = Vehicle('B', 3, (4, 3), "down", g)
-    carC = Vehicle('C', 2, (5, 1), "down", g)
-    carD = Vehicle('D', 2, (4, 6), "left", g)
-    special = Vehicle('S', 2, (2, 3), "right", g)
+    loadToGrid(inPath, g)
     g.printGrid()
-    print g.isFinished()
+    Solver = Search(g)
+    moves = Solver.aStarSearch()
+    if moves != []:
+        print ("Solved in %i moves" % len(moves))
+        if printSolutions:
+            Solver.printSolution(moves)
+        writeToFile(outPath, moves)
+    else:
+        print "No Solution Found"
 
 if __name__ == "__main__":
     main()
